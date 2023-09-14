8081145cc1bfc8dbcff0a6b4612d45a38fc02c40
Brightidada <brightidada28@gmail.com>
2023-09-14 08:07:08 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
+
 def arithmetic_arranger(problems, show_result=False):
-    if len(problems) > 8:
+    if len(problems) > 5: 
         return "Error: Too many problems."
 
     arranged_problems = []
@@ -14,14 +15,18 @@ def arithmetic_arranger(problems, show_result=False):
 
         max_len = max(len(num1), len(num2))
         arranged_problem = f"{num1:>{max_len}}\n{operator} {num2:>{max_len}}"
+        arranged_problem_string=str(arranged_problem)
         line = '-' * (max_len + 2)
         result = eval(problem) if show_result else ""
-        arranged_problems.append((arranged_problem, line, str(result).rjust(max_len + 2)))
 
-    arranged = '    '.join(arranged_problems)
+        arranged_problems.append((arranged_problem_string, line, str(result).rjust(max_len + 2)))
+
+    arranged = ''.join(arranged_problems)
     if show_result:
-        return arranged + '\n' + '    '.join([line for _, line, _ in arranged_problems])
+        return arranged + '\n' + ''.join([line for _, line, _ in arranged_problems])
     else:
         return arranged
 
-print (arithmetic_arranger('32 + 45',True))
+problemss=['39 - 65',    '70 + 677']
+answer= arithmetic_arranger(problemss)
+print (answer)
